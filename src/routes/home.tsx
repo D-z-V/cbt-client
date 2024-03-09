5d4697b0661b4f9bfaca88b3ac0acbbf73e5e73c
Dev Bhandari <dev.bhandari@hotmail.com>
2024-03-09 10:06:03 +0530

    ui

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -7,10 +7,21 @@ import reactLogo from './assets/react.svg';
 import authressLogo from './assets/logo.svg';
 import Openapi from './openapi';
 
+import React from 'react';
+import Button from '@mui/material/Button';
+import TestPage from '../../components/testportal';
+
 function App() {
   const navigate = useNavigate();
   const [userProfile, setUserProfile] = useState({});
   const [authressApiUrlIsSet, setAuthressApiUrlIsSet] = useState(true);
+
+    const [showTestPage, setShowTestPage] = useState<boolean>(false);
+  
+    const handleCreateTaskClick = () => {
+      setShowTestPage(true);
+    };
+  
 
   // let userProfile;
   useEffect(() => {
@@ -30,61 +41,14 @@ function App() {
     setAuthressApiUrlIsSet(starterKitIsConfiguredCorrectly);
   }, []);
 
+
   return (
-    <div className="App">
-
-      
-      <h1>Social CBT</h1>
-
-      {/* <Openapi /> */}
-      <div
-        style={{
-          border: '1px white solid',
-          borderRadius: '10px',
-          padding: '2rem',
-        }}
-      >
-        {authressApiUrlIsSet ? (
-          <div>
-            {!userProfile ? 
-              <button style={{ marginRight: '1rem' }} onClick={login}>
-                Login
-              </button>
-              : <button style={{ marginRight: '1rem' }} onClick={logout}>
-                Logout
-              </button>
-            }
-            <button style={{ marginRight: '1rem' }} onClick={() => navigate('/protected')}>
-              Go to Authress Protected Route Page
-            </button>
-
-            {userProfile ?
-              <button style={{ marginRight: '1rem' }} onClick={makeApiCallWithUserToken}>
-                Call your service API
-              </button>
-              : ''
-            }
-
-            <br></br>
-            <br></br>
-
-            <div style={{ textAlign: 'left' }}>
-              <span>User Profile:</span>
-              <pre style={{ whiteSpace: 'break-spaces', overflowWrap: 'break-word' }}>{JSON.stringify(userProfile || 'Not logged in', null, 2)}</pre>
-            </div>
-
-            <br></br>
-          </div>
-        ) : (
-          <div>
-            <div>
-              Authress Account Host URL is missing from your configuration.<br></br>Specify the <strong>AuthressApiUrl</strong> at the top of the <code style={{ color: '#dc3545' }}>src/authressClient.tsx</code> file.
-              <br /><br />
-              If you need an account, sign up for free at <a href="https://authress.io/app/#/signup">Authress.io</a>!
-            </div>
-          </div>
-        )}
-      </div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      {!showTestPage ? (
+        <Button variant="contained" onClick={handleCreateTaskClick}>Create Task</Button>
+      ) : (
+        <TestPage />
+      )}
     </div>
   );
 }
