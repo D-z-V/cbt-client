81c1ed2b870df867b302cdde80438062961bb6d4
Gaurav <hibyehello68@gmail.com>
2024-03-09 12:04:49 +0530

    Complete create test portal

--- components/testportal.tsx
+++ components/testportal.tsx
@@ -6,30 +6,70 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
+import Textarea from '@mui/joy/Textarea';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 enum InputMethod {
   MANUAL = 'manual',
   PDF = 'pdf',
 }
 
+interface Question {
+  question: string;
+  options: string[];
+}
+
+interface Section {
+  name: string; 
+  marks: number;
+  schema: string;
+  questionType: string;
+  questions: Question[];
+}
+
 const TestPage: React.FC = () => {
   const [selectedInputMethod, setSelectedInputMethod] = useState<InputMethod>(InputMethod.PDF); // Set default to PDF
-  const [numQuestions, setNumQuestions] = useState<number>(1);
-  const [questions, setQuestions] = useState<{ question: string, options: string[] }[]>([]);
+  const [sections, setSections] = useState<Section[]>([]);
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const [showNumQuestionsInput, setShowNumQuestionsInput] = useState(true);
-  const [showAddQuestion, setShowAddQuestion] = useState(false);
+  const [showAddSection, setShowAddSection] = useState(true);
+  const [currentSection, setCurrentSection] = useState<number>(0);
+  const [manualInstructions, setManualInstructions] = useState<string>('');
+  const [pdfInstructions, setPdfInstructions] = useState<string>('');
 
-  const handleInputChange = (index: number, value: string) => {
-    const newQuestions = [...questions];
-    newQuestions[index].question = value;
-    setQuestions(newQuestions);
+  const handleAddSection = () => {
+    setSections([
+      ...sections,
+      {
+        name: `Section ${sections.length + 1}`,
+        marks: 0,
+        schema: '',
+        questionType: '',
+        questions: [],
+      }
+    ]);
   };
 
-  const handleOptionChange = (questionIndex: number, optionIndex: number, value: string) => {
-    const newQuestions = [...questions];
-    newQuestions[questionIndex].options[optionIndex] = value;
-    setQuestions(newQuestions);
+  const handleInputChange = (sectionIndex: number, questionIndex: number, value: string) => {
+    const newSections = [...sections];
+    newSections[sectionIndex].questions[questionIndex].question = value;
+    setSections(newSections);
+  };
+
+  const handleOptionChange = (sectionIndex: number, questionIndex: number, optionIndex: number, value: string) => {
+    const newSections = [...sections];
+    newSections[sectionIndex].questions[questionIndex].options[optionIndex] = value;
+    setSections(newSections);
+  };
+
+  const handleManualSubmit = () => {
+    console.log('Manual Instructions:', manualInstructions);
+    console.log('Manual Answers:', sections);
+  };
+
+  const handlePDFSubmit = () => {
+    console.log('PDF Instructions:', pdfInstructions);
+    console.log('Selected file:', selectedFile);
   };
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -38,42 +78,29 @@ const TestPage: React.FC = () => {
     }
   };
 
-  const handleManualSubmit = () => {
-    console.log('Manual Answers:', questions);
+  const handleAddQuestion = (sectionIndex: number) => {
+    const newSections = [...sections];
+    newSections[sectionIndex].questions.push({
+      question: '',
+      options: ['', '', '', ''],
+    });
+    setSections(newSections);
   };
 
-  const handlePDFSubmit = () => {
-    console.log('Selected file:', selectedFile);
-  };
-
-  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedInputMethod(event.target.value as InputMethod);
-    setShowNumQuestionsInput(event.target.value === InputMethod.MANUAL);
-    setShowAddQuestion(event.target.value === InputMethod.MANUAL);
-  };
-
-  const handleAddQuestion = () => {
-    setNumQuestions(numQuestions + 1);
-    setQuestions([
-      ...questions,
-      {
-        question: '',
-        options: ['', '', '', ''],
-      }
-    ]);
-  };
+  // Calculate the width of the box dynamically based on the number of sections
+  const boxWidth = `${sections.length * 200 + 400}px`; // Adjust the multiplier and add extra padding as needed
 
   return (
-    <Box width={800} mx="auto" p={4} border="0.5px solid grey" borderRadius={4}>
+    <Box width={boxWidth} mx="auto" p={4} border="0.5px solid #ccc" borderRadius={4} bgcolor="#f7f7f7" color="#000">
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <h1>Create Test</h1>
       </div>
       <Box mb={2}>
         <RadioGroup
           aria-label="inputMethod"
-          defaultValue={InputMethod.PDF} // Set default value to PDF
+          defaultValue={InputMethod.PDF} 
           value={selectedInputMethod}
-          onChange={handleRadioChange}
+          onChange={(e) => setSelectedInputMethod(e.target.value as InputMethod)}
           row
         >
           <FormControlLabel
@@ -88,44 +115,124 @@ const TestPage: React.FC = () => {
           />
         </RadioGroup>
       </Box>
-      {selectedInputMethod === InputMethod.MANUAL && showAddQuestion && (
-        <Box>
-          {questions.map((question, index) => (
-            <Box key={index} mb={4}>
-              <TextField
-                id={`question-${index}`}
-                label={`Question ${index + 1}`}
-                variant="outlined"
-                value={question.question}
-                onChange={(e) => handleInputChange(index, e.target.value)}
-              />
-              <br />
-              <br />
-              <Grid container spacing={2}>
-                {[...Array(4).keys()].map((optionIndex) => (
-                  <Grid item xs={3} key={optionIndex}>
-                    <TextField
-                      id={`option-${index}-${optionIndex}`}
-                      label={`Option ${optionIndex + 1}`}
-                      variant="filled"
-                      value={question.options[optionIndex]}
-                      onChange={(e) => handleOptionChange(index, optionIndex, e.target.value)}
-                    />
-                  </Grid>
-                ))}
+      {selectedInputMethod === InputMethod.MANUAL && (
+        <>
+          <Textarea
+            placeholder="Add instructions for manual input here..."
+            value={manualInstructions}
+            onChange={(e) => setManualInstructions(e.target.value)}
+            rows={4}
+            style={{ width: '100%', marginBottom: '10px' }}
+          />
+          <Tabs value={currentSection} onChange={(e, newValue) => setCurrentSection(newValue)} textColor="primary">
+            {sections.map((section, index) => (
+              <Tab key={index} label={section.name} />
+            ))}
+            <Button variant="contained" onClick={handleAddSection} style={{ marginLeft: 'auto', backgroundColor: '#4caf50', color: '#fff' }}>Add Section</Button>
+          </Tabs>
+          {sections.map((section, sectionIndex) => (
+            <div key={sectionIndex} hidden={sectionIndex !== currentSection}>
+              <h2>{section.name}</h2>
+              <Grid container spacing={2} alignItems="center">
+                <Grid item xs={3}>
+                  <TextField
+                    label="Section Name" // Add section name field
+                    variant="outlined"
+                    value={section.name}
+                    onChange={(e) => {
+                      const newSections = [...sections];
+                      newSections[sectionIndex].name = e.target.value;
+                      setSections(newSections);
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={3}>
+                  <TextField
+                    label="Marks"
+                    variant="outlined"
+                    value={section.marks}
+                    onChange={(e) => {
+                      const newSections = [...sections];
+                      const marks = parseInt(e.target.value);
+                      newSections[sectionIndex].marks = isNaN(marks) ? 0 : marks; // Check for NaN
+                      setSections(newSections);
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={3}>
+                  <TextField
+                    label="Schema"
+                    variant="outlined"
+                    value={section.schema}
+                    onChange={(e) => {
+                      const newSections = [...sections];
+                      newSections[sectionIndex].schema = e.target.value;
+                      setSections(newSections);
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={3}>
+                  <TextField
+                    label="Question Type"
+                    variant="outlined"
+                    value={section.questionType}
+                    onChange={(e) => {
+                      const newSections = [...sections];
+                      newSections[sectionIndex].questionType = e.target.value;
+                      setSections(newSections);
+                    }}
+                  />
+                </Grid>
               </Grid>
-            </Box>
+              {section.questions.map((question, questionIndex) => (
+                <Box key={questionIndex} mt={2}>
+                  <Textarea
+                    placeholder={`Question ${questionIndex + 1}...`}
+                    minRows={2}
+                    style={{ width: "100%" }}
+                    value={question.question}
+                    onChange={(e) => handleInputChange(sectionIndex, questionIndex, e.target.value)}
+                  />
+                  <br />
+                  <Grid container spacing={2}>
+                    {[...Array(4).keys()].map((optionIndex) => (
+                      <Grid item xs={3} key={optionIndex}>
+                        <TextField
+                          id={`option-${sectionIndex}-${questionIndex}-${optionIndex}`}
+                          label={`Option ${optionIndex + 1}`}
+                          variant="outlined"
+                          value={question.options[optionIndex]}
+                          onChange={(e) => handleOptionChange(sectionIndex, questionIndex, optionIndex, e.target.value)}
+                        />
+                      </Grid>
+                    ))}
+                  </Grid>
+                </Box>
+              ))}
+              <br />
+              <Button variant="contained" onClick={() => handleAddQuestion(sectionIndex)} style={{ backgroundColor: '#4caf50', color: '#fff' }}>Add Question</Button>
+            </div>
           ))}
-          <Button variant="contained" onClick={handleAddQuestion} style={{ marginRight: '10px' }}>Add Question</Button>
-          <Button variant="contained" onClick={handleManualSubmit}>Submit</Button>
-        </Box>
+          <Box mt={2} display="flex" justifyContent="flex-end">
+            <Button variant="contained" onClick={handleManualSubmit} style={{ marginRight: '10px', backgroundColor: '#2196f3', color: '#fff' }}>Submit</Button>
+          </Box>
+        </>
       )}
       {selectedInputMethod === InputMethod.PDF && (
-        <div>
-          <h2>Upload PDF</h2>
-          <input type="file" accept="application/pdf" onChange={handleFileChange} />
-          <Button  variant="contained" onClick={handlePDFSubmit} disabled={!selectedFile}>Submit</Button>
-        </div>
+        <>
+          <Textarea
+            placeholder="Add instructions for PDF upload here..."
+            value={pdfInstructions}
+            onChange={(e) => setPdfInstructions(e.target.value)}
+            rows={4}
+            style={{ width: '100%', marginBottom: '10px' }}
+          />
+          <div>
+            <h2>Upload PDF</h2>
+            <input type="file" accept="application/pdf" onChange={handleFileChange} />
+            <Button variant="contained" onClick={handlePDFSubmit} disabled={!selectedFile} style={{ backgroundColor: '#2196f3', color: '#fff' }}>Submit</Button>
+          </div>
+        </>
       )}
     </Box>
   );
